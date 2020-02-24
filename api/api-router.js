58894e733bbf3a54fc16ff657ed11042fd66d7e4
aaronjan <aaronjan98@gmail.com>
2020-02-24 13:27:08 -0800

    get to /hash

--- api/api-router.js
+++ api/api-router.js
@@ -5,19 +5,19 @@ const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
 const restricted = require("../auth/restricted-middleware.js");
 
-// router.use("/auth", authRouter);
-// router.use("/users", restricted, usersRouter);
+router.use("/auth", authRouter);
+router.use("/users", restricted, usersRouter);
 
-// router.get("/hash", (req, res) => {
-//   // read the Authentication header
-//   const authentication = req.headers.authentication;
+router.get("/hash", (req, res) => {
+  // read the Authentication header
+  const authentication = req.headers.authentication;
 
-//   // hash the value from that header
-//   const hash = bcrypt.hashSync(authentication, 8);
+  // hash the value from that header
+  const hash = bcrypt.hashSync(authentication, 8);
 
-//   res.json({ originalValue: authentication, hashedValue: hash });
+  res.json({ originalValue: authentication, hashedValue: hash });
 
-// });
+});
 
 router.get("/", (req, res) => {
   res.json({ api: "It's alive" });

7a40a7a0fe11607599724141f4a7ceb972552cab
aaronjan <aaronjan98@gmail.com>
2020-02-25 15:14:10 -0800

    implemented logout endpoint

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -41,4 +41,20 @@ router.post("/login", (req, res) => {
     });
 });
 
+router.get("/logout", (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.status(500).json({
+            you: "can check out any time you like, but you can never leave",
+          });
+        } else {
+          res.status(200).json({ you: "logged out successfully" });
+        }
+      });
+    } else {
+      res.status(200).json({ bye: "felicia" });
+    }
+});
+
 module.exports = router;
